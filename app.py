8d5bcf17b928f346c02d2396d0c0f5b8e8c1ccd6
rollexxyz <yadavjijmb@gmail.com>
2025-07-26 10:54:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Tushar'
 
-
 if __name__ == "__main__":
-    app.run()
+    # Get the port from environment variable provided by Render
+    port = int(os.environ.get('PORT', 5000))  # fallback to 5000 for local testing
+    app.run(host='0.0.0.0', port=port)
